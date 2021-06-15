33b90dc6b4881dc4c9cfd891de5161a5116db99c
weberstills <adamoommenjacob@gmail.com>
2021-06-15 22:35:43 +0530

    scope

--- es6variable.js
+++ es6variable.js
@@ -2,3 +2,19 @@ let num=10;
  num=20;
 
 console.log(num)
+
+
+function hello(){
+
+    i=0;
+    if(i===0){
+        let hey=2;  
+        console.log(hey) 
+    }
+    
+}
+
+hello()
+
+//let has scope only inside block
+//var has scope just outside also
