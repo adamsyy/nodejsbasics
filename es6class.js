787e69bedad4fb7d6685501bc25377b25cfcd9f3
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-06-16 23:29:06 +0530

    es6class

---
+++ es6class.js
@@ -0,0 +1,16 @@
+class Hello{
+
+    constructor(num1,num2){
+        this.num1=num1
+        this.num2=num2
+        
+    }
+   
+    
+    helo(){
+        console.log('ok'+this.num1+this.num2)
+    }
+}
+
+ oi=new Hello(10,210)
+ oi.helo()
