9e90b4009db263265dd0fc46a142dace90574f6a
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-06-16 23:31:27 +0530

    super cons

--- es6class.js
+++ es6class.js
@@ -1,6 +1,15 @@
-class Hello{
+class samp{
+
+    poo(){
+        console.log('poo')
+    }
+}
+
+
+class Hello extends samp{
 
     constructor(num1,num2){
+        super()
         this.num1=num1
         this.num2=num2
         
@@ -13,4 +22,4 @@ class Hello{
 }
 
  oi=new Hello(10,210)
- oi.helo()
+ oi.poo()
