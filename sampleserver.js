471ba4b16ce8af416c09870a0e4e09cd7d10a181
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-06-17 13:13:20 +0530

    server

--- sampleserver.js
+++ sampleserver.js
@@ -1,8 +1,13 @@
+
+var fs=require('fs')
 var http=require('http')
 
 http.createServer(function(req,res){
-
-    res.write('node server')
-    res.end()
+     fs.readFile('one.html',function(err,data){
+         res.writeHead(200,{'content-Type':'text/html'})
+        res.write(data)
+        res.end()
+     })
+    
    }).listen(7000)
 
